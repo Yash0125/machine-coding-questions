0b47ae980fffb70aed1652b11d0525241124d10e
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-08-26 15:37:25 +0530

    feat : todolist added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import "./App.css";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
-    <div>
+    <div className="app">
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+function TodoList() {
+  const [todo, setTodo] = useState(["React", "Javascript"]);
+  const [text, setText] = useState("");
+  const [editIndex, setEditIndex] = useState(-1);
+
+  const addTodo = () => {
+    const newTodo = [...todo, text];
+    setTodo(newTodo);
+    setText("");
+  };
+
+  const deleteTodo = (index) => {
+    const newTodo = [...todo];
+    newTodo.splice(index, 1);
+    setTodo(newTodo);
+  };
+
+  const editTodo = (index) => {
+    setEditIndex(index);
+    setText(todo[index]);
+  };
+
+  const saveTodo = () => {
+    const newTodo = [...todo];
+    newTodo[editIndex] = text;
+    setTodo(newTodo);
+    setEditIndex(-1);
+    setText("");
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        placeholder="add todo"
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button onClick={addTodo}>Add TODO</button>
+
+      <ul>
+        {todo.map((item, index) => (
+          <>
+            <li key={index}>
+              {editIndex === index ? (
+                <input
+                  type="text"
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                />
+              ) : (
+                item
+              )}
+              <button onClick={() => deleteTodo(index)}>delete</button>
+              {editIndex === index ? (
+                <button onClick={() => saveTodo()}>save</button>
+              ) : (
+                <>
+                  <button onClick={() => editTodo(index)}>edit</button>
+                </>
+              )}
+            </li>
+          </>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
