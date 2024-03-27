c96a56ca979bfd6558d6894d47724965560e8865
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2024-03-27 19:26:26 +0530

    refactor :  starRating 2nd approach added

--- src/components/starRating/StarRating.jsx
+++ src/components/starRating/StarRating.jsx
@@ -13,8 +13,35 @@ const StarRating = () => {
       {[...new Array(5).keys()].map((i)=>(
         <span key={i} data={i + 1} className={i < rating ? "star rated" : "star"}></span>
       ))}
+      <RatingMachine/>
     </div>
   )
 }
 
 export default StarRating
+
+const RatingMachine = () => {
+  const [rating, setRating] = useState(0);
+
+  const handleStarClick = (value) => {
+    setRating(value);
+  };
+
+  return (
+    <div>
+      <h2>Rate this Machine:</h2>
+      <div>
+        {[1, 2, 3, 4, 5].map((value) => (
+          <span
+            key={value}
+            // className={value <= rating ? 'star filled' : 'star'}
+            onClick={() => handleStarClick(value)}
+          >
+            {value <= rating ? '★' : '☆'}
+          </span>
+        ))}
+      </div>
+      <p>You rated this machine: {rating}</p>
+    </div>
+  );
+};
